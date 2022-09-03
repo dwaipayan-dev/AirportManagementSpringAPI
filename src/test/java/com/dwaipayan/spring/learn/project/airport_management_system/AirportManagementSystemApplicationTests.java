8b6fbda0bd991fe5d7980f136231368b0d749472
dwaipayan-dev <dwaipayan.vidyanta1998@gmail.com>
2022-09-03 23:15:42 +0530

    Setup commit

---
+++ src/test/java/com/dwaipayan/spring/learn/project/airport_management_system/AirportManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dwaipayan.spring.learn.project.airport_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirportManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
